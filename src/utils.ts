1eef5763b105a1856ffee95cd2b9d80739f9ccff
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2021-07-28 08:24:14 +0900

    全然動かないのでもっと汚くした

--- src/utils.ts
+++ src/utils.ts
@@ -10,9 +10,15 @@ export function toInt(str: string) {
   if (str != null) return parseInt(str.replace(/\D+/g, ""));
 }
 
+function allChildren(root: Element): Element[] {
+  return [root, ...[...root.children].flatMap(allChildren)];
+}
+
 /** 要素のitemprop属性を雑に連想配列として取得 */
 export function getItemProps(e: Element) {
-  const itemprops = [...e.querySelectorAll("[itemprop]")] as Element[];
+  // なんか`e.querySelectorAll("[itemprop]")`にしたいけど、なんか持ってきてくれないことがある
+  // ちょっとアレだけど動くからいいや
+  const itemprops = allChildren(e).filter((e) => e.hasAttribute("itemprop"));
 
   const kv = itemprops.map((prop) => {
     const key = prop.getAttribute("itemprop");
@@ -20,6 +26,7 @@ export function getItemProps(e: Element) {
     let value;
     value ||= prop.getAttribute("href");
     value ||= prop.getAttribute("src");
+    value ||= prop.getAttribute("datetime");
     value ||= prop.textContent;
     value = value.trim();
 
